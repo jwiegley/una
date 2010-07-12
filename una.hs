750137c96fc8c049f367423b541b12451a87808c
John Wiegley <johnw@newartisans.com>
2010-07-12 04:43:45 -0400

    Removed an obsolete comment

--- una.hs
+++ una.hs
@@ -475,14 +475,10 @@ extractByTemp :: B.ByteString   -- output to write to temp
 extractByTemp ds ext fn = do
   dir <- workingDirectory
   (path, handle) <- openBinaryTempFile dir ("file" ++ ext)
-
   loud <- isLoud
   when loud $ putStrLn $ "> " ++ path
-
   B.hPut handle ds
   hFlush handle
-  -- unzip does not support reading from standard input, so we must write the
-  -- data out to a temporary file in order to continue.
   (x,m) <- fn (FileName path)
   hClose handle
   return (x, do removeFile path; m)
