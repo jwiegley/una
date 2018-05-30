8a610b774d63c2f1b6f34f7bb2d04368a046956a
John Wiegley <johnw@newartisans.com>
2018-05-30 11:08:22 -0700

    Update default.nix files for profiling

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
 { compiler    ? "ghc822" # "ghc842" also works
-, doProfiling ? false
 , doBenchmark ? false
 , doTracing   ? false
 , doStrict    ? false
@@ -24,9 +23,6 @@ in haskellPackages.developPackage {
   };
 
   modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
-    enableLibraryProfiling    = doProfiling;
-    enableExecutableProfiling = doProfiling;
-
     inherit doBenchmark;
   });
 
