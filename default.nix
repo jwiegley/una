8c1ed9bcda7e0e84df00247340514d3b857fc1a8
John Wiegley <johnw@newartisans.com>
2025-01-03 16:35:04 -0800

    Add flake.nix file

--- default.nix
+++ default.nix
@@ -1,31 +1,7 @@
-{ compiler    ? "ghc8106"
-, doBenchmark ? false
-, doTracing   ? false
-, doStrict    ? false
-, rev         ? "24c765c744ba856700db92ab94ef9c695d73f95f"
-, sha256      ? "0ak482k4jsnnmipmc038fla5ywr9h01xs91sjkx35wkkxcs2lc23"
-, pkgs        ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256; }) {
-    config.allowUnfree = true;
-    config.allowBroken = false;
-  }
-, returnShellEnv ? pkgs.lib.inNixShell
-, mkDerivation ? null
-}:
-
-let haskellPackages = pkgs.haskell.packages.${compiler};
-
-in haskellPackages.developPackage {
-  root = ./.;
-
-  source-overrides = {
-  };
-
-  modifier = drv: pkgs.haskell.lib.overrideCabal
-    (pkgs.haskell.lib.justStaticExecutables drv) (attrs: {
-    inherit doBenchmark;
-  });
-
-  inherit returnShellEnv;
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
+    sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
+) {
+  src =  ./.;
+}).defaultNix
